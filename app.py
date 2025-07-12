8d92f540e929013cbfb31a1525d5f68cdbcb8958
naylazahs <naeylazzz@gmail.com>
2025-07-12 09:03:06 +0700

    app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.write("Hello, *World!* :sunglasses:")
+
+st.title("This is a title 😁")
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+
+st.header("_Streamlit_ is :blue[cool] :sunglasses:", divider="gray")
+
+st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+
+st.markdown("*Streamlit* is **really** ***cool***.")
